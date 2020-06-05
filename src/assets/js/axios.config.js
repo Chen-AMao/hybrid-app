2f9a46c1c06ae8bbdb112f97b620e155af34b3e9
Chen-AMao <ansi6@126.com>
2020-06-05 17:39:56 +0800

    feature: axios config

--- src/assets/js/axios.config.js
+++ src/assets/js/axios.config.js
@@ -3,7 +3,24 @@ import Vue from 'vue'
 import axios from 'axios'
 
 // 设置axios请求baseUrl
-axios.defaults.baseURL = 'https://easy-mock.com/mock/5ceb51b551e3de74af53fe54/imooc/api'
+// axios.defaults.baseURL = 'https://easy-mock.com/mock/5ceb51b551e3de74af53fe54/imooc/api'
+axios.defaults.baseURL = 'http://api.imooc.hybrid.lgdsunday.club/'
+
+/**
+ * 所有使用axios发送的请求，在请求发送出去之前，都会进入到这个方法
+ * 在这里，为所有的请求添加上token
+ */
+axios.interceptors.request.use(config => {
+  if (config.params) {
+    config.params.token = '76576b10-a70f-11ea-ad19-03481aa896cd'
+  } else {
+    config.params = {
+      token: '76576b10-a70f-11ea-ad19-03481aa896cd'
+    }
+  }
+  // 不要忘记config对象需要return
+  return config
+})
 
 /**
  * 设置拦截器，interceptors响应处理，
@@ -13,10 +30,13 @@ axios.defaults.baseURL = 'https://easy-mock.com/mock/5ceb51b551e3de74af53fe54/im
  */
 
 axios.interceptors.response.use((response) => {
-  // 统一处数据
-  return response.data
+  // 统一处数据， 使组件中的请求只获取到需要的业务数据，
+  // 而不需要去关注返回的状态码等与业务无关的数据
+  console.log('interceptors response')
+  return response.data.data
 }, (error) => {
   // 处理错误的响应
+  console.log('interceptors error', error)
   return Promise.reject(error)
 })
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="home">
     <div class="home-content">
-        <my-swiper :swiperImgs="swiperImgs" :height="swiperHeight"></my-swiper>
+        <my-swiper :swiperImgs="swiperData.map(item => item.icon)" :height="swiperHeight"></my-swiper>
     </div>
 </div>
 </template>
@@ -16,16 +16,33 @@ export default {
   data: function () {
     return {
       swiperImgs: [
-        require('@imgs/swiper-1.jpg'),
-        require('@imgs/swiper-2.jpg'),
-        require('@imgs/swiper-3.jpg'),
-        require('@imgs/swiper-4.jpg'),
-        require('@imgs/swiper-5.jpg'),
-        require('@imgs/swiper-6.jpg'),
-        require('@imgs/swiper-7.jpg'),
-        require('@imgs/swiper-8.jpg')
+        // require('@imgs/swiper-1.jpg'),
+        // require('@imgs/swiper-2.jpg'),
+        // require('@imgs/swiper-3.jpg'),
+        // require('@imgs/swiper-4.jpg'),
+        // require('@imgs/swiper-5.jpg'),
+        // require('@imgs/swiper-6.jpg'),
+        // require('@imgs/swiper-7.jpg'),
+        // require('@imgs/swiper-8.jpg')
       ],
+      swiperData: [],
       swiperHeight: '184px'
+    }
+  },
+  created: function () {
+    this.initData()
+  },
+  methods: {
+    // 获取数据
+    initData: function () {
+      // this.$http = axios
+      this.$http.get('swiper')
+        .then(data => {
+          console.log(data)
+          this.swiperData = data.list
+        }).catch(err => {
+          console.log(err)
+        })
     }
   }
 }
